e6d0cab81ea82604a48027a94f0eef661b5e1e85
Eduard Ilyaskin <ilyaskin1998@gmail.com>
2018-06-06 01:50:53 +0600

    add target course 1

---
+++ src/components/UI/Target.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Span1 = styled.span`
+  font-size: 20px;
+  background-color: #999;
+  color: white;
+  line-height: 50px;
+  width: 92px;
+  padding-left: 5px;
+`;
+
+const Span2 = styled.span`
+  font-size: 20px;
+  margin-left: 10px;
+`;
+
+const DivBorder = styled.div`
+  border: 1px solid #618AD2;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  border-radius: 3px;
+  background-color: rgba(245,245,245,0.95);
+`;
+
+const Target = props => {
+  return (
+    <DivBorder>
+      <Span1>
+        - {props.step}
+      </Span1>
+      <Span2>
+        {props.children}
+      </Span2>
+    </DivBorder>
+  );
+};
+
+export default Target;
